a2020178ebf7db77e3daa2ec1e1e1ffe64209db5
Arnav Motwani <arnavmotwani77@gmail.com>
2021-02-20 12:49:06 +0530

    Initial Commit

---
+++ jsonExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  jsonExample
+//
+//  Created by Arnav Motwani on 20/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ jsonExample/jsonExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  jsonExampleApp.swift
+//  jsonExample
+//
+//  Created by Arnav Motwani on 20/02/21.
+//
+
+import SwiftUI
+
+@main
+struct jsonExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
